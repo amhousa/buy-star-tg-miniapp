d0eba7865d6d18e5c8e2418ee849fc253e5ed891
Amirhossein Salmani <amirhossein.salmani2@gmail.com>
2024-11-29 05:12:10 +0330

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     outDir: 'docs'
   },
